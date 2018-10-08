9da8ad54c85b64f353b6abfb175ce59b85307b12
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2018-10-08 12:42:08 +0000

    Update license header

--- src/errors.rs
+++ src/errors.rs
@@ -1,9 +1,5 @@
 // Chemfiles, a modern library for chemistry file reading and writing
-// Copyright (C) 2015-2017 Guillaume Fraux
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/
+// Copyright (C) 2015-2018 Guillaume Fraux -- BSD licensed
 extern crate libc;
 
 use std::error;
