1daa0331dd565a8b3c6389246f98e5a9b0f83564
Luthaf <luthaf@luthaf.fr>
2022-08-25 12:53:03 +0200

    Update to chemfiles 0.10.3

--- chemfiles-sys/prebuilt.rs
+++ chemfiles-sys/prebuilt.rs
@@ -1,21 +1,21 @@
-/// Get the julia triple & sha256 corresponding to the prebuilt libchemfiles
+/// Get the julia triple & sha256 corresponding to the prebuilt chemfiles v0.10.3
 /// for a given rust triple, if it exists
 pub fn get_prebuilt_info(target: &str) -> Option<(&'static str, &'static str)> {
     match target {
-        "aarch64-apple-darwin" => Some(("aarch64-apple-darwin", "de8fc3c7534cc9077f91e276524ca53e482a69de966df21965508ba71ee3d21a")),
-        "aarch64-unknown-linux-gnu" => Some(("aarch64-linux-gnu", "8fd043575a198745cfc6993714dc754aa4ce0fae20743f326137b292cb7292fa")),
-        "aarch64-unknown-linux-musl" => Some(("aarch64-linux-musl", "e8d40230bfc8e37910e6766b27f2751598dc2c79d27d93d9b9377625add91e94")),
-        "armv7-unknown-linux-gnueabihf" => Some(("armv7l-linux-gnueabihf", "98ced72ad195129c8045733fd63fe3573910c5fcf2fe0565d9dd445c0b4ee538")),
-        "armv7-unknown-linux-musleabihf" => Some(("armv7l-linux-musleabihf", "2306657577e0c33a8f562ea72579dd4785feea6df9fcfd76e4f104f3804d4a3c")),
-        "i686-unknown-linux-gnu" => Some(("i686-linux-gnu", "297ab506dbdac6dd6febedf544c276475988cff0273b455c5cd9cba7e23805f2")),
-        "i686-unknown-linux-musl" => Some(("i686-linux-musl", "ef29187fed702472178186acfb0e6fddedbc1cd5511cf50b0e976509c0582cad")),
-        "i686-pc-windows-gnu" => Some(("i686-w64-mingw32", "a206dc9ce00b998c8996a7fcf4d3de3ac6d487af950dd9d53fb9009c12b7e860")),
-        "powerpc64le-unknown-linux-gnu" => Some(("powerpc64le-linux-gnu", "7b87fc0c303652ad28101cd0d75ebd03a44cd303918da855c2f40cebac313208")),
-        "x86_64-apple-darwin" => Some(("x86_64-apple-darwin", "6490efd0fef41c4014a48406fe89d30c464bb9857194bc6cf6012fa807acfa71")),
-        "x86_64-unknown-linux-gnu" => Some(("x86_64-linux-gnu", "16cf6f38f817e555d0393db2726ab80b3b6ff1b2d8f884478f938eda9a452371")),
-        "x86_64-unknown-linux-musl" => Some(("x86_64-linux-musl", "8d0ebba7b42cb7bcc123a0ffd118a73046eea19f47f298d790f9dbd88937b4f1")),
-        "x86_64-unknown-freebsd" => Some(("x86_64-unknown-freebsd", "4d2ecca4109198f37c416bf3f5d85cf6d21bd5d3fb1d08c2b0559405d951bfcc")),
-        "x86_64-pc-windows-gnu" => Some(("x86_64-w64-mingw32", "f622c55ff6b20b0c3e2433f2f28dff8b81e863785a1c295f25066b0953eb08fe")),
+        "aarch64-apple-darwin" => Some(("aarch64-apple-darwin", "d363921687c3d9a292a680567f3244cd1159f2c17a4b897361d7703a954b3d31")),
+        "aarch64-unknown-linux-gnu" => Some(("aarch64-linux-gnu", "503b5a384cbc627d6dfa0e3c38923fd4b881619aa2c58d8d37257fb00d593b73")),
+        "aarch64-unknown-linux-musl" => Some(("aarch64-linux-musl", "26da29259916f1b57c567a7e4ad72fa113065d69b2c131a0de92ed77bdc1c943")),
+        "armv7-unknown-linux-gnueabihf" => Some(("armv7l-linux-gnueabihf", "14e950a8e10c2e1e3408c9fbfee0da463a978f3c139fc9ddb6f79323c7eab48d")),
+        "armv7-unknown-linux-musleabihf" => Some(("armv7l-linux-musleabihf", "444d30b2c82c1ba9f9be2c0ba5092b3d86bfbb02f995935490ab12bea0767423")),
+        "i686-unknown-linux-gnu" => Some(("i686-linux-gnu", "d4d4833269b7589e8035b5718236c40477969c3c281b20cdb3a9f09eeb291396")),
+        "i686-unknown-linux-musl" => Some(("i686-linux-musl", "b8914c51fe06fd1848ceaf0b5666e05ff4b1dd3edb05dea26b9bbc99d51426e3")),
+        "i686-pc-windows-gnu" => Some(("i686-w64-mingw32", "f4cae7e8fcca7d32e539f9c080dfd950cf5b835ffa3d75aae7dd16f597829573")),
+        "powerpc64le-unknown-linux-gnu" => Some(("powerpc64le-linux-gnu", "af7adf574ddc953593dda2e89b39d71c6674e44cd8a3fc729edc1f48b9f01542")),
+        "x86_64-apple-darwin" => Some(("x86_64-apple-darwin", "0783cb15705576f4b5af6bed583540a256a83a906d160dc6f157b7d5f594579b")),
+        "x86_64-unknown-linux-gnu" => Some(("x86_64-linux-gnu", "0a2a839885effae7df0c279ad024b6fa7cb73666c2c2fe9b6675cebc614b088e")),
+        "x86_64-unknown-linux-musl" => Some(("x86_64-linux-musl", "866edb9fb201e05b24c04d115ba9cb27e5381f94bf5637c7409e43b4513ba4a4")),
+        "x86_64-unknown-freebsd" => Some(("x86_64-unknown-freebsd", "51f659041fb65f43038541dabb2dfa47712bb0ec08ea57065954e42334d9f797")),
+        "x86_64-pc-windows-gnu" => Some(("x86_64-w64-mingw32", "e183dfbef91e5ce608be7ae8c3ca902982ab0f5b7276d4049958cbc75c0155c6")),
         _ => None,
     }
 }
